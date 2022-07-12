2ec37b43bfc80f0c8ea8d36b4b7909fc1508b264
TommyBspeed <103615463+TommyBspeed@users.noreply.github.com>
2022-07-12 15:43:12 -0600

    Add files via upload

---
+++ javascriptV2.js
@@ -0,0 +1,28 @@
+function changeHeight() {
+  document.getElementById("box").style.height="250px"
+}
+
+function makeBlue() {
+    document.getElementById("box").style.backgroundColor="blue"
+}
+
+function makeDim() {
+    document.getElementById("box").style.opacity=".4"
+}
+
+function tiny() {
+    document.getElementById("box").style.height="30px"
+    document.getElementById("box").style.width="30px"
+}
+
+function outline() {
+    document.getElementById("box").style.outline=" thick solid black"
+}
+
+function Reset() {
+    document.getElementById("box").style.height="150px"
+    document.getElementById("box").style.width="150px"
+    document.getElementById("box").style.backgroundColor="orange"
+    document.getElementById("box").style.opacity="1.0"
+    document.getElementById("box").style.outline="none"
+}
